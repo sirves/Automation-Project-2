d993b97cff5654dd803466c1eb72926400f2f1d0
sirves <sirve25@gmail.com>
2024-02-23 19:21:51 +0200

    My commets about the commit

--- cypress/e2e/e2e-pom/issue-delete-in-pom.cy.js
+++ cypress/e2e/e2e-pom/issue-delete-in-pom.cy.js
@@ -16,14 +16,12 @@ describe('Issue delete', () => {
   const issueTitle = 'This is an issue of type: Task.';
 
   it('Should delete issue successfully', () => {
-    //add steps to delete issue
     IssueModal.clickDeleteButton
     IssueModal.confirmDeletion
     IssueModal.ensureIssueIsNotVisibleOnBoard
   });
 
   it('Should cancel deletion process successfully', () => {
-    //add steps to start deletion proces but cancel it
     IssueModal.ensureIssueIsVisibleOnBoard
     IssueModal.clickDeleteButton
     IssueModal.cancelDeletion
